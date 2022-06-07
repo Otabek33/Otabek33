2376244c3c46547dc3a558fd957fab5aa5e8b442
Otabek <kebatotabek33@gmail.com>
2022-06-07 12:27:17 +0500

    just commit

---
+++ Main.java
@@ -0,0 +1,10 @@
+/**
+ * @author "Davronov Otabek"
+ * @since 6/7/2022 12:21 PM (Tuesday)
+ * Git/IntelliJ IDEA
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Nimadir");
+    }
+}
